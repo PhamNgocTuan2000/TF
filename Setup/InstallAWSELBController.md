999881e9e0549105391bc6ac6f8083e752f28b15
Phamtuan <luckylukes2k@gmail.com>
2024-10-10 21:10:34 +0700

    push eks

--- Setup/InstallAWSELBController.md
+++ Setup/InstallAWSELBController.md
@@ -2,29 +2,29 @@ Ref: https://docs.aws.amazon.com/eks/latest/userguide/lbc-helm.html
 
 add  iam open id
 
-eksctl utils associate-iam-oidc-provider --region=us-west-2 --cluster=qnveks --profile vti --approve
+eksctl utils associate-iam-oidc-provider --region=us-west-2 --cluster=phamtuan-eks --profile default --approve
 
 add service account
 
 eksctl create iamserviceaccount \
-  --cluster=qnveks \
+  --cluster=phamtuan-eks \
   --namespace=kube-system \
   --name=aws-load-balancer-controller \
   --role-name AmazonEKSLoadBalancerControllerRole \
   --attach-policy-arn=arn:aws:iam::084375555299:policy/AWSLoadBalancerControllerIAMPolicy \
   --approve \
-  --profile vti \
-  --region us-west-2
+  --profile default \
+  --region ap-northeast-1
 
 add helm
 
 helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
   -n kube-system \
-  --set clusterName=qnveks \
+  --set clusterName=phamtuan-eks \
   --set serviceAccount.create=false \
   --set serviceAccount.name=aws-load-balancer-controller \
-  --set region=us-west-2 \
-  --set vpcId=vpc-0c2a1525239b24fd9
+  --set region=ap-northeast-1 \
+  --set vpcId=vpc-04fb10db0c9888f36
 
 verify
 kubectl get deployment -n kube-system aws-load-balancer-controller
--- Setup/assigsusertoeks.sh
+++ Setup/assigsusertoeks.sh
@@ -2,12 +2,16 @@
 
 #references document: https://docs.aws.amazon.com/eks/latest/userguide/access-policies.html
 
-AWS_REGION=ap-northeast-1
-AWS_PROFILE=default
-EKS_CLUSTER_NAME=phamtuan-eks
-AWS_USER_ARN=arn:aws:iam::084375555299:user/DE000014
-TYPE_ACCESS=cluster
-POLICY_ARN=arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy
+AWS_REGION=ap-northeast-1 \
+AWS_PROFILE=default \
+EKS_CLUSTER_NAME=phamtuan-eks \
+AWS_USER_ARN=arn:aws:iam::084375555299:user/DE000014 \
+TYPE_ACCESS=cluster \
+POLICY_ARN=arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy \
 
-echo "setup user to access eks cluster"
-aws eks associate-access-policy --cluster-name $EKS_CLUSTER_NAME --region $AWS_REGION  --profile $AWS_PROFILE --principal-arn $AWS_USER_ARN --access-scope type=$TYPE_ACCESS  --policy-arn $POLICY_ARN
+echo "Create the access to eks cluster"
+
+aws eks create-access-entry --cluster-name phamtuan-eks --principal-arn arn:aws:iam::084375555299:user/DE000014 --type STANDARD --user DE000014  --kubernetes-groups Viewers --profile default --region ap-northeast-1
+
+ 
+aws eks associate-access-policy --cluster-name $EKS_CLUSTER_NAME --profile $AWS_PROFILE --region $AWS_REGION --principal-arn $AWS_USER_ARN  --access-scope type=cluster --policy-arn arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy
